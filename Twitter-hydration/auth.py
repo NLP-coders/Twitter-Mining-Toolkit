ca75d051afc58e924ee2e36e16ef727f25dd9ff2
rsalshalan <rsalshalan@gmail.com>
2020-08-11 03:46:03 +0300

    move files to correct directory

---
+++ Twitter-hydration/auth.py
@@ -0,0 +1,6 @@
+class TwitterAuthKeys:
+        # You first need to create an app via http://dev.twitter.com 
+        consumer_key = "112"
+        consumer_secret = "YOURKEYGOESHERE"
+        access_token = "YOURKEYGOESHERE"
+        access_token_secret = "YOURKEYGOESHERE"
