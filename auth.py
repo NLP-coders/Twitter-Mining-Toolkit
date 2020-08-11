e4f37cd7beaa7819897032c462cd738bde3e1600
rshalan <Rsalshalan@gmail.com>
2020-08-11 03:49:29 +0300

    Delete auth.py

--- auth.py
+++
@@ -1,6 +0,0 @@
-class TwitterAuthKeys:
-        # Yoi first need to create an app via http://dev.twitter.com 
-        consumer_key = "YOURKEYGOESHERE"
-        consumer_secret = "YOURKEYGOESHERE"
-        access_token = "YOURKEYGOESHERE"
-        access_token_secret = "YOURKEYGOESHERE"
