5a22749417e444f200ea186a363a5dd94af572ee
rsalshalan <rsalshalan@gmail.com>
2020-08-26 01:51:30 +0300

    main skelton for taking args

--- Twitter-hydration/main.py
+++ Twitter-hydration/main.py
@@ -1,7 +1,16 @@
 from config import Config
-import mysql.connector
+from tweepy import TweepError
+#import mysql.connector
+import argparse
+
+#https://levelup.gitconnected.com/the-easy-guide-to-python-command-line-arguments-96b4607baea1
+parser = argparse.ArgumentParser(description='Parse the  hydrator script')
+parser.add_argument("--a")
+
+args = parser.parse_args()
+a = args.a
+print(a)
+
 
 #Creat mysql connection 
 #db = mysql.connector.connect(host=Config.host , user = Config.user, password = Config.password)
-
-print("In main script")
